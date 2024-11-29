4b06937e42a2a7c7555320ab77634279c9d92e0c
andreasLoetzsch <andreas.lotzsch@gmail.com>
2024-11-29 13:29:12 +0100

    feat: file structur and created model/user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 require('dotenv').config();
 const PORT = process.env.PORT
 
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port: ${PORT}`);
