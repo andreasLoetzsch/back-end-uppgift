4dc924f1f643b6ea449ba0c883f3dcca5252cd3c
andreasLoetzsch <andreas.lotzsch@gmail.com>
2024-12-01 13:08:05 +0100

    feat: token created with middleware auth

--- public/scripts/formSubmit.js
+++ public/scripts/formSubmit.js
@@ -1,19 +1,43 @@
+const content = document.getElementById('content')
 
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault(); 
+document.getElementById('login-btn').addEventListener('click', function(e) {
+    e.preventDefault()
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const token = localStorage.getItem('user')
 
-    axios.post('http://localhost:3000/api/user/login', { username, password })
+    axios.post('http://localhost:3000/api/getHtml/sign-in',{}, { headers: {Authorization: token} })
         .then(response => {
-            const token = response.data.token;
+            while (content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
             console.log(response)
-            localStorage.setItem('user', token); 
-            alert('Login successful!');
+            content.innerHTML = response.data
+            const loginForm = document.getElementById('loginForm')
+            console.log(loginForm)
+            if(loginForm){
+                loginForm.addEventListener('submit', function (e) {
+                    e.preventDefault(); 
+                
+                    const username = document.getElementById('username').value;
+                    const password = document.getElementById('password').value;
+                
+                    axios.post('http://localhost:3000/api/user/login', { username, password })
+                        .then(response => {
+                            const token = response.data.token;
+                            console.log(response)
+                            localStorage.setItem('user', token); 
+                        })
+                        .catch(error => {
+                            console.error('Error logging in:', error);
+                            alert('Login failed. Please try again.');
+                        });
+                });
+            }
+            
         })
         .catch(error => {
             console.error('Error logging in:', error);
             alert('Login failed. Please try again.');
         });
-});
+})
+
--- src/routes/getHtmlRoutes.js
+++ src/routes/getHtmlRoutes.js
@@ -4,9 +4,9 @@ const express = require('express');
 const router = express.Router()
 const path = require('path')
 
-router.get('/sign-in', authUser, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/signin.html'))
-    // res.json({user: req.user})
+router.post('/sign-in', authUser, (req, res) => {
+    // res.sendFile(path.join(__dirname, '../../public/signin.html'))
+    res.json({user: req.user})
 });
 
 module.exports = router
